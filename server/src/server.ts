938a406ac659aa89cacefd58a1e8df6f1eeb4fe3
tsunoda1 <tsunoda@b2ml.com.br>
2020-06-28 16:31:48 -0300

    create API

---
+++ server/src/server.ts
@@ -0,0 +1,25 @@
+// importandoo biblioteca express
+import express from "express";
+import cors from "cors";
+import routes from "./routes";
+
+const port = 3333;
+
+// declarando uma constante para receber o express
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+//rota padrão
+app.get("/", function (req, res) {
+  res.send("API - Cervejeiros [v.1 - 2020 | 28/06/2020]");
+});
+
+// Definindo porta 3333 para rodar a API
+app.listen(port, function () {
+  console.log('Servidor API "Cervejeiros" [v.1 - 2020 | 28/06/2020] => ATIVO [PORTA: 3333]');
+});
+
+
